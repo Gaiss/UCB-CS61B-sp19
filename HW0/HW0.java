c9ddf073f3836e171371cebc11f01e5e3aa5cf12
Gaiss <54195329+Gaiss@users.noreply.github.com>
2020-04-26 16:58:33 +0800

    Update HW0.java

--- HW0/HW0.java
+++ HW0/HW0.java
@@ -26,6 +26,26 @@ public class ClassNameHere {
 public class ClassNameHere {
    public static int max(int[] m) {
       int getmax = m[0];
+      int i = 1;
+      while(i<m.length){
+         if(m[i]>getmax){
+            getmax = m[i];
+         }
+         i += 1;
+      }
+      return getmax;
+   }
+   public static void main(String[] args) {
+      int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6}; 
+      System.out.println(max(numbers));
+   }
+}
+
+
+/* EX3 */
+public class ClassNameHere {
+   public static int forMax(int[] m) {
+      int getmax = m[0];
       for(int i=1;i<m.length;i=i+1){
          if(m[i]>getmax){
             getmax = m[i];
