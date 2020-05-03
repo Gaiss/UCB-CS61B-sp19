634da081728c75dcaf86c105262bf032cb7af110
Gaiss <54195329+Gaiss@users.noreply.github.com>
2020-05-03 12:47:24 +0800

    Update HW0.java

--- HW0/HW0.java
+++ HW0/HW0.java
@@ -52,7 +52,7 @@ public class ClassNameHere {
    }
    public static void main(String[] args) {
       int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6}; 
-      System.out.println(max(numbers));
+      System.out.println(forMax(numbers));
    }
 }
 
